d189d755d581d4574bef56468af690448fb206b4
Carl Medina <carl.l.medina@comcast.net>
2019-09-18 18:28:09 -0700

    setup db folder, created schema and seends for burgers database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('All-Star', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BurgerIM', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Flamethrower', true);
